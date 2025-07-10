a8b3d2cf973d5a073356fb3c63812294e57ca844
Muhammad Shahzaib <shahzaib.shahid686@gmail.com>
2025-07-11 00:55:04 +0500

    Section added in the project contact us

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
+
 import Footer from "./Footer/page";
 import Navbar from "./Navbar/Navbar";
-
+import ContactUs from "./ContactUs/page"
 export default function Home() {
   return (
     <div>
       <Navbar />
+    <ContactUs/>
       <Footer/>
-    </div>
+      
+     </div>
   );
 }
